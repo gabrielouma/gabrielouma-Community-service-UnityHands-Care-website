5c203a7a38d3d52c5e71737b1b2bb29d2e5b43f3
Gabriel Ouma <oumagaby01@gmail.com>
2024-09-11 00:50:45 +0300

    debugged the whole code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
-function toggle() {
-    var header = document.querySelector("#header")
-    header.classList.toggle('active1')
+// function toggle() {
+//     var header = document.querySelector("#header")
+//     header.classList.toggle('active1')
+// }
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
 }
